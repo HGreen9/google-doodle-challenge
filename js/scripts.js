a5418a902d4089e9c1f55fbcea8aa65245ba1711
Hannah <156165697+HGreen9@users.noreply.github.com>
2025-02-14 06:04:49 +0000

    *added Gsap animations *Fought for my life with custom confetti, gave up

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,16 @@
 // console.log('OK');
 console.log(gsap);
+
+gsap.from(".Letters", {
+  y: -61,
+  opacity: 0,
+  delay: 1,
+  duration: 3, 
+  ease: 'elastic'
+});
+
+gsap.from(".Vines", {
+  opacity: 0, 
+  duration: 3, // Fixed typo here
+  delay: 3.2
+});
